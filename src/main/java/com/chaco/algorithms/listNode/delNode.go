d4ec9dbceb9c29e1d9b4f8df521455a53b9ad3c1
zhaopeiyan <zhaopeiyan@meican.com>
2022-10-19 16:22:09 +0800

    del listnode golang

--- src/main/java/com/chaco/algorithms/listNode/delNode.go
+++ src/main/java/com/chaco/algorithms/listNode/delNode.go
@@ -3,14 +3,13 @@ package listNode
 func deleteNode(head *ListNode, val int) *ListNode {
 	res := &ListNode{}
 	res.Next = head
-	cur := head
-	for p := res; nil != p.Next; p = p.Next {
+	p := res
+	for cur := head; nil != cur.Next; cur = cur.Next {
 		if val == p.Next.Val {
 			p.Next = cur.Next
 			break
 		}
 		p = cur
-		cur = cur.Next
 	}
 	return res.Next
 }
