6b18edc5f2f9b510c70e3d08f87e1ed643a79fb0
zhaopeiyan <zhaopeiyan@meican.com>
2022-10-19 18:01:57 +0800

    avg 5% array golang

---
+++ src/main/java/com/chaco/algorithms/array/trimMean.go
@@ -0,0 +1,15 @@
+package array
+
+import (
+	"sort"
+)
+
+func trimMean(arr []int) float64 {
+	sort.Ints(arr)
+	n := len(arr)
+	sum := 0
+	for _, x := range arr[n/20 : 19*n/20] {
+		sum += x
+	}
+	return float64(sum*10) / float64(n*9)
+}
