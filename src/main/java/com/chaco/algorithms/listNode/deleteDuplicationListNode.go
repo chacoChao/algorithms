7baaa642dca2db70b448df253f245ca71cff211f
zhaopeiyan <zhaopeiyan@meican.com>
2022-10-21 14:56:19 +0800

    del listnode go

---
+++ src/main/java/com/chaco/algorithms/listNode/deleteDuplicationListNode.go
@@ -0,0 +1,19 @@
+package listNode
+
+func deleteDuplication(pHead *ListNode) *ListNode {
+	if nil == pHead {
+		return nil
+	}
+	res := &ListNode{}
+	res.Next = pHead
+	for pre := res; nil != pre.Next && nil != pre.Next.Next; {
+		if pre.Next.Val == pre.Next.Next.Val {
+			tmp := pre.Next.Val
+			for ; nil != pre.Next && tmp == pre.Next.Val; pre.Next = pre.Next.Next {
+			}
+		} else {
+			pre = pre.Next
+		}
+	}
+	return res.Next
+}
