fea01bf0005cafd6c564df89d86e4eb9f6b07982
zhaopeiyan <zhaopeiyan@meican.com>
2022-10-24 14:34:46 +0800

    cycle ndoe

---
+++ src/main/java/com/chaco/algorithms/listNode/entryNodeOfLoop.go
@@ -0,0 +1,22 @@
+package listNode
+
+func EntryNodeOfLoop(pHead *ListNode) *ListNode {
+	fir := pHead
+	sec := pHead
+	for nil != sec.Next && nil != sec.Next.Next {
+		fir = fir.Next
+		sec = sec.Next.Next
+		if fir == sec {
+			break
+		}
+	}
+	if nil == sec.Next || nil == sec.Next.Next {
+		return nil
+	}
+	fir = pHead
+	for fir != sec {
+		fir = fir.Next
+		sec = sec.Next
+	}
+	return fir
+}
