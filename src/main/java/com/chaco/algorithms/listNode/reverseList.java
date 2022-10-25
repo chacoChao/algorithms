d473a0ea510b8258a1145881184c65b3d72c8a2e
zhaopeiyan <zhaopeiyan@meican.com>
2022-10-25 15:04:21 +0800

    反转链表之递归解法 update doc

--- src/main/java/com/chaco/algorithms/listNode/reverseList.java
+++ src/main/java/com/chaco/algorithms/listNode/reverseList.java
@@ -75,10 +75,29 @@ public class reverseList {
     /**
      * 递归
      */
-    public ListNode ReverseList3(ListNode head) {
+    public static ListNode ReverseList3(ListNode head) {
         if (null == head || null == head.next) {
             return head;
         }
+//第一轮出栈，head为5，head.next为空，返回5
+//第二轮出栈，head为4，head.next为5，执行head.next.next=head也就是5.next=4，
+//把当前节点的子节点的子节点指向当前节点
+//此时链表为1->2->3->4<->5，由于4与5互相指向，所以此处要断开4.next=null
+//此时链表为1->2->3->4<-5
+//返回节点5
+//第三轮出栈，head为3，head.next为4，执行head.next.next=head也就是4.next=3，
+//此时链表为1->2->3<->4<-5，由于3与4互相指向，所以此处要断开3.next=null
+//此时链表为1->2->3<-4<-5
+//返回节点5
+//第四轮出栈，head为2，head.next为3，执行head.next.next=head也就是3.next=2，
+//此时链表为1->2<->3<-4<-5，由于2与3互相指向，所以此处要断开2.next=null
+//此时链表为1->2<-3<-4<-5
+//返回节点5
+//第五轮出栈，head为1，head.next为2，执行head.next.next=head也就是2.next=1，
+//此时链表为1<->2<-3<-4<-5，由于1与2互相指向，所以此处要断开1.next=null
+//此时链表为1<-2<-3<-4<-5
+//返回节点5
+//出栈完成，最终头节点5->4->3->2->1
         ListNode newHead = ReverseList3(head.next);
         head.next.next = head;
         head.next = null;
@@ -101,6 +120,20 @@ public class reverseList {
             newHead = node;
         }
         return newHead;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(3);
+        ListNode l4 = new ListNode(4);
+        ListNode l5 = new ListNode(5);
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        l4.next = l5;
+        ListNode cycle = reverseList.ReverseList3(l1);
+        System.out.println("========" + cycle.val);
     }
 }
 
