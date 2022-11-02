39bfb2abe324312b89137564a04607dccf5ef463
zhaopeiyan <zhaopeiyan@meican.com>
2022-11-02 19:31:09 +0800

    ad teenode.java

---
+++ src/main/java/com/chaco/algorithms/TreeNode.java
@@ -0,0 +1,18 @@
+package com.chaco.algorithms;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@ToString
+public class TreeNode {
+    public int val = 0;
+    public TreeNode left = null;
+    public TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
