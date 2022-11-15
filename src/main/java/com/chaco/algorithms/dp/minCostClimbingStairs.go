9cb35226171268b5edc9460c433ee6a9b6d30151
zhaopeiyan <zhaopeiyan@meican.com>
2022-11-15 16:44:08 +0800

    爬楼最小代价

---
+++ src/main/java/com/chaco/algorithms/dp/minCostClimbingStairs.go
@@ -0,0 +1,13 @@
+package dp
+
+import "math"
+
+func minCostClimbingStairs(cost []int) int {
+	dp := make([]int, len(cost)+1)
+	dp[0] = 0
+	dp[1] = 0
+	for i := 2; i <= len(cost); i++ {
+		dp[i] = int(math.Min(float64(dp[i-1]+cost[i-1]), float64(dp[i-2]+cost[i-2])))
+	}
+	return dp[len(cost)]
+}
