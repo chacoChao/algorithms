fceb78ad5b4c3662756e71f810381f3b62d16e16
zhaopeiyan <zhaopeiyan@meican.com>
2022-11-18 17:10:20 +0800

    二叉搜索树第K个节点 java

---
+++ src/main/java/com/chaco/algorithms/tree/KthNode.java
@@ -0,0 +1,44 @@
+package com.chaco.algorithms.tree;
+
+import com.chaco.algorithms.TreeNode;
+
+/**
+ * https://www.nowcoder.com/practice/57aa0bab91884a10b5136ca2c087f8ff?tpId=13&tqId=2305268&ru=/exam/oj/ta&qru=/ta/coding-interviews/question-ranking&sourceUrl=%2Fexam%2Foj%2Fta%3Fpage%3D1%26tpId%3D13%26type%3D13
+ * 给定一棵结点数为n 二叉搜索树，请找出其中的第 k 小的TreeNode结点值。
+ * 1.返回第k小的节点值即可
+ * 2.不能查找的情况，如二叉树为空，则返回-1，或者k大于n等等，也返回-1
+ * 3.保证n个节点的值不一样
+ * 进阶：空间复杂度 O(n)O(n)，时间复杂度 O(n)O(n)
+ * 如输入{5,3,7,2,4,6,8},3时，二叉树{5,3,7,2,4,6,8}如下图所示：
+ * 该二叉树所有节点按结点值升序排列后可得[2,3,4,5,6,7,8]，所以第3个结点的结点值为4，故返回对应结点值为4的结点即可。
+ */
+public class KthNode {
+    public static void main(String[] args) {
+        //[3,1,4,null,2]
+        TreeNode t4 = new TreeNode(4);
+        TreeNode t3 = new TreeNode(3);
+        TreeNode t2 = new TreeNode(2);
+        TreeNode t1 = new TreeNode(1);
+        t3.left = t1;
+        t1.right = t2;
+        t3.right = t4;
+        int i = KthNode.KthNode(t3, 4);
+        System.out.println("KthNode.main:" + i);
+    }
+
+    static int res = -1;
+    static int count = 0;
+
+    public static int KthNode(TreeNode proot, int k) {
+        if (null == proot || k < 0) {
+            return -1;
+        }
+        KthNode(proot.left, k);
+        count++;
+        if (count == k) {
+            return res = proot.val;
+        }
+        KthNode(proot.right, k);
+        return res;
+    }
+}
