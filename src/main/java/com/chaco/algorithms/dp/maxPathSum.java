a8f5888b3e0878a928186932f67969b09529c784
zhaopeiyan <zhaopeiyan@meican.com>
2023-01-03 17:27:32 +0800

    二叉树中的最大路径和

---
+++ src/main/java/com/chaco/algorithms/dp/maxPathSum.java
@@ -0,0 +1,31 @@
+package com.chaco.algorithms.dp;
+
+import com.chaco.algorithms.TreeNode;
+
+/**
+ * https://www.nowcoder.com/practice/8fda1d22554f4824b0ef9c26f35e54dd?tpId=230&tqId=39756&ru=/exam/oj
+ * https://leetcode.cn/problems/binary-tree-maximum-path-sum/
+ * 路径 被定义为一条从树中任意节点出发，沿父节点-子节点连接，达到任意节点的序列。
+ * 同一个节点在一条路径序列中 至多出现一次 。该路径 至少包含一个 节点，且不一定经过根节点。
+ * <p>
+ * 路径和 是路径中各节点值的总和。
+ * <p>
+ * 给你一个二叉树的根节点 root ，返回其 最大路径和 。
+ * <p>
+ * 输入：root = [1,2,3]
+ * 输出：6
+ * 解释：最优路径是 2 -> 1 -> 3 ，路径和为 2 + 1 + 3 = 6
+ * <p>
+ * 输入：root = [-10,9,20,null,null,15,7]
+ * 输出：42
+ * 解释：最优路径是 15 -> 20 -> 7 ，路径和为 15 + 20 + 7 = 42
+ */
+public class maxPathSum {
+    public static void main(String[] args) {
+        System.out.println("maxPathSum.main");
+    }
+
+    public int maxPathSum(TreeNode root) {
+        return 0;
+    }
+}
