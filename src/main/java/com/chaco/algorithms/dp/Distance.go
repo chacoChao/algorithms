50476e3437961f5eab482571e9bc6c3854355d32
zhaopeiyan <zhaopeiyan@meican.com>
2023-01-13 15:42:48 +0800

    distance golang

--- src/main/java/com/chaco/algorithms/dp/Distance.go
+++ src/main/java/com/chaco/algorithms/dp/Distance.go
@@ -49,25 +49,25 @@ func Max(args ...int) int {
 }
 
 func minDistance1(word1 string, word2 string) int {
-	n1, n2 := len(word1), len(word2)
-	dp := make([][]int, n1+1)
-	for i := range dp {
-		dp[i] = make([]int, n2+1)
-	}
-	for i := 1; i < len(word2); i++ {
-		dp[0][i] = dp[0][i-1] + 1
-	}
-	for j := 1; j < len(word1); j++ {
-		dp[j][0] = dp[j-1][0] + 1
-	}
-	for i := 1; i <= len(word1); i++ {
-		for j := 1; j <= len(word2); j++ {
-			if word1[i-1] == word2[j-1] {
-				dp[i][j] = dp[i-1][j-1]
-			} else {
-				dp[i][j] = Min(dp[i-1][j-1], Min(dp[i][j-1], dp[i-1][j])) + 1
-			}
-		}
-	}
-	return dp[n1][n2]
+    n1, n2 := len(word1), len(word2)
+    dp := make([][]int, n1+1)
+    for i := range dp {
+        dp[i] = make([]int, n2+1)
+    }
+    for i := 1; i <= len(word2); i++ {
+        dp[0][i] = dp[0][i-1] + 1
+    }
+    for j := 1; j <= len(word1); j++ {
+        dp[j][0] = dp[j-1][0] + 1
+    }
+    for i := 1; i <= len(word1); i++ {
+        for j := 1; j <= len(word2); j++ {
+            if word1[i-1] == word2[j-1] {
+                dp[i][j] = dp[i-1][j-1]
+            } else {
+                dp[i][j] = Min(dp[i-1][j-1], dp[i][j-1], dp[i-1][j]) + 1
+            }
+        }
+    }
+    return dp[n1][n2]
 }
